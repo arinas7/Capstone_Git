af3b65abb99c6d7d4ae3b4070bc06657137727f6
Jin Yang <deblang7@hotmail.com>
2014-09-17 12:21:06 -0400

    Start

---
+++ Admin_Web/Admin_Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Admin_Web.Startup))]
+namespace Admin_Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
